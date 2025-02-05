8411a0a804d26563fdd6590320b71209479a62bb
Gali Sai Mohan Reddy <gali.s@jmangroup.com>
2025-02-05 17:48:09 +0530

    Changes Saved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="flex items-center space-x-4 ml-4">
         
         <img
-          src="src\assets\blog.png" 
+          src="src/assets/blog.png" 
           alt="Logo"
           className="w-12 h-12"
         />
